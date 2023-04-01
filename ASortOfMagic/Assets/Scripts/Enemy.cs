bb7429133e27c2028a4381590da15cf619fb8a2d
magicstunts123 <78170077+magicstunts123@users.noreply.github.com>
2023-04-01 20:11:37 +0200

    Update Enemy.cs
    CheckForPlayer

--- ASortOfMagic/Assets/Scripts/Enemy.cs
+++ ASortOfMagic/Assets/Scripts/Enemy.cs
@@ -8,6 +8,15 @@ public abstract class Enemy : MonoBehaviour
     public int health = 100;
     
     public abstract void InflictDamage(PizzaBullet bullet);
+    public GameObject Player;
+    public float radius = 5f;
+    public float angle = 180f;
+
+
+    private void Update()
+    {
+        checkForPlayer();
+    }
 
     public virtual void InflictDamage(int damage)
     {
@@ -23,4 +32,16 @@ public abstract class Enemy : MonoBehaviour
     {
         Destroy(gameObject);
     }
+
+    void checkForPlayer()
+    {
+        Vector2 directionToPlayer = Player.transform.position - transform.position;
+        float angleToPlayer = Vector2.Angle(Vector2.up, directionToPlayer);
+        if (directionToPlayer.magnitude <= radius && angleToPlayer <= angle / 2f)
+        {
+            Debug.Log("Player in sight");
+        }
+    }
+
+    
 }
