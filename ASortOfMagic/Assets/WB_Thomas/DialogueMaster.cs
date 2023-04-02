1b8a5615fed2f80ca8b322588c16de9d63aeaf2c
magicstunts123 <78170077+magicstunts123@users.noreply.github.com>
2023-04-02 23:22:09 +0200

    PickUp/DialogBox

--- ASortOfMagic/Assets/WB_Thomas/DialogueMaster.cs
+++ ASortOfMagic/Assets/WB_Thomas/DialogueMaster.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class DialogueMaster : MonoBehaviour
@@ -22,11 +23,21 @@ public class DialogueMaster : MonoBehaviour
     }
 
     public GameObject popUpBox;
+    public GameObject imageBox;
     public Animator popUpAnimator;
     public TMP_Text popUpText;
+    public Sprite defaultSprite;
 
-    public void PopUpText(string text)
+    public void PopUpText(string text, bool useImage, Sprite image)
     {
+        if (useImage)
+        {
+            imageBox.GetComponent<Image>().sprite = image;
+        }
+        else
+        {
+            imageBox.GetComponent<Image>().sprite = defaultSprite;
+        }
         popUpBox.SetActive(true);
         popUpText.text = text;
         popUpAnimator.SetTrigger("pop");
